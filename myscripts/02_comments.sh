98ce8389c0b4d17116ce96fd5b8a119df3140303
root <root@Pitabash.localdomain>
2026-07-27 23:52:47 +0530

    Initial commit

---
+++ myscripts/02_comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#This is a single line comment
+
+echo "Checking Comments"
+
+<<comment
+This 
+is 
+multi
+line
+comments
+comment
+
+
---
+++ myscripts/03_variable_demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Script to show how to use variables
+
+a = 10
+name = "Pitabash"
+age = 28
+
+echo "My name is $name and my age is $age"
+
